d48e9878f2f5db457be4ff3822d53698d8483179
X Yan <ansonyxr@gmail.com>
2021-03-10 16:29:04 -0800

    update port

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ import Cors from "cors";
 
 // App config
 const app = express();
-const port = process.env.port || 8001;
+const port = process.env.PORT || 8001;
 const connection_url =
   "mongodb+srv://admin:0emArebHv8ig2htl@cluster0.mxapj.mongodb.net/tinder-db?retryWrites=true&w=majority";
 
